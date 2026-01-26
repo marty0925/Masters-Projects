2cc3c82714ff21b2e15032115cfe1b310ab786ac
Martin D <marty274@Hotmail.com>
2026-01-26 10:53:41 +0000

    Update Getting_data.sh

--- Extended Research Project/Originals/STAR_Index/Getting_data.sh
+++ Extended Research Project/Originals/STAR_Index/Getting_data.sh
@@ -3,3 +3,7 @@ wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/genco
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/GRCm39.primary_assembly.genome.fa.gz #For FastA file
 
 #The link where the data was found was: https://www.gencodegenes.org/mouse/
+#Then we need to use gunzip to be unzip both of the downloaded files before being about to use them for indexing
+
+gunzip gencode.vM38.primary_assembly.annotation.gtf.gz
+gunzip GRCm39.primary_assembly.genome.fa.gz
