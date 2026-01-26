2d2d5402e3ba5cde0b333a8714de9cfa14bb1fb7
Martin D <marty274@Hotmail.com>
2026-01-26 10:30:28 +0000

    Create Getting_data.sh

---
+++ Extended Research Project/Originals/STAR_Index/Getting_data.sh
@@ -0,0 +1,5 @@
+#This is the code used to get the GteX and Fasta file that were used to generate the index
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/gencode.vM38.primary_assembly.annotation.gtf.gz #for the GteX File
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/GRCm39.primary_assembly.genome.fa.gz #For FastA file
+
+#The link where the data was found was: https://www.gencodegenes.org/mouse/
