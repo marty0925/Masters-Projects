5ef1a9a5ec0ffcee55d7fde57b8def8f14ab0aeb
Martin D <marty274@Hotmail.com>
2026-01-27 11:17:54 +0000

    Update Create_Smaller_Metadata_File.sh

--- Extended Research Project/Metadata/Create_Smaller_Metadata_File.sh
+++ Extended Research Project/Metadata/Create_Smaller_Metadata_File.sh
@@ -1,4 +1,4 @@
 #This file has the code which was used to cut down the original larger CSV metadata file to have only the SRR_IDs and the ReadType
-cut -d ',' -f 1,26 SraRunTable.csv > SRR_ReadType.csv
+cut -d ',' -f 1, 26 SraRunTable.csv | tail -n +2 > SRR_ReadType.csv
 
 #The column "LibraryLayout", which contains information about whether the reads were single or paired-end, was selected using column 26, despite being in column 19, because cut reads every comma in the file.  
